efebdbd3c8252a780446c6b3692c7afc6af291ab
TheNewby <alx.care@gmail.com>
2023-12-03 23:12:43 +0100

    day 1 : part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,39 @@
 advent_of_code::solution!(1);
 
+#[macro_use]
+extern crate icecream;
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    // ic!(input);
+
+    let mut v: u32 = 0;
+    let mut s_first: Option<char> = None;
+    let mut s_last: Option<char> = None;
+
+    for c in input.chars() {
+        if c.is_ascii_digit() {
+            match s_first {
+                None => s_first = Some(c),
+                Some(_) => s_last = Some(c),
+            }
+        } else if c.is_ascii_control() {
+            let d = s_first.unwrap().to_digit(10).unwrap();
+
+            match s_last {
+                None => {
+                    v += d * 10 + d;
+                }
+                Some(val) => {
+                    let u: u32 = val.to_digit(10).unwrap();
+                    v += d * 10 + u;
+                }
+            }
+            s_first = None;
+            s_last = None;
+        }
+    }
+
+    Some(v)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +47,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(142));
     }
 
     #[test]
