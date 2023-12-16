055cb069179189b8d1fca0fa201e61492c183e6f
TheNewby <alx.care@gmail.com>
2023-12-16 13:09:12 +0100

    day 3 part two

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -70,7 +70,78 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    // println!("{}", input);
+    let re_number = Regex::new(r"([\d]+)").unwrap();
+    let re_special = Regex::new(r"(\*)").unwrap();
+
+    let line_size = input.lines().next().unwrap().len();
+
+    let numbers: Vec<regex::Match<'_>> = re_number.find_iter(input).collect();
+
+    // let v = re_special.find(input);
+
+    // dbg!(v);
+
+    let v: u32 = re_special
+        .find_iter(input)
+        .map(|map| {
+            // dbg!(map);
+            // dbg!(map.start(), map.end() + 1, map.start() - 11,);
+
+            // println!(
+            //     "{}{}{}\n{}{}{}\n{}{}{}\n",
+            //     input.chars().nth(map.start() - line_size - 2).unwrap(),
+            //     input.chars().nth(map.start() - line_size - 1).unwrap(),
+            //     input.chars().nth(map.start() - line_size).unwrap(),
+            //     input.chars().nth(map.start() - 1).unwrap(),
+            //     input.chars().nth(map.start()).unwrap(),
+            //     input.chars().nth(map.end()).unwrap(),
+            //     input.chars().nth(map.end() + line_size - 1).unwrap(),
+            //     input.chars().nth(map.end() + line_size).unwrap(),
+            //     input.chars().nth(map.end() + line_size + 1).unwrap()
+            // );
+            numbers
+                .iter()
+                .filter_map(move |num| {
+                    if (map.start() - line_size - 2 <= num.start()
+                        && num.start() <= map.start() - line_size)
+                        || (map.start() - line_size - 2 < num.end()
+                            && num.end() - 1 <= map.start() - line_size
+                            || num.end() == map.start()
+                            || num.start() == map.end()
+                            || (map.end() + line_size - 1 <= num.start()
+                                && num.start() <= map.end() + line_size + 1)
+                            || (map.end() + line_size - 1 < num.end()
+                                && num.end() - 1 <= map.end() + line_size + 1))
+                    {
+                        return Some(num.as_str().parse::<u32>().unwrap());
+                    // } else if num.end() == map.start() || num.start() == map.end() {
+                    //     return Some(num.as_str().parse::<u32>().unwrap());
+                    // }
+                    // if (map.end() + line_size - 1 <= num.start()
+                    //     && num.start() <= map.end() + line_size + 1)
+                    //     || (map.end() + line_size - 1 < num.end()
+                    //         && num.end() - 1 <= map.end() + line_size + 1)
+                    // {
+                    //     return Some(num.as_str().parse::<u32>().unwrap());
+                    } else {
+                        // println!("NOT FOUND {}", num.as_str());
+                        None
+                    }
+                })
+                .collect::<Vec<_>>()
+        })
+        .fold(0, |sum, ve| {
+            if ve.len() == 2 {
+                return sum + ve[0] * ve[1];
+            }
+            sum
+        });
+
+    // v.sort();
+    // dbg!(v);
+
+    Some(v)
 }
 
 #[cfg(test)]
@@ -86,6 +157,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
