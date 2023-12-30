b4fcdb982f27d20a9d0ed948d926df147e927f19
TheNewby <alx.care@gmail.com>
2023-12-30 13:45:55 +0100

    day 8: part 2 slow

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,8 +1,10 @@
-use std::collections::HashMap;
+use std::{borrow::Borrow, collections::HashMap, vec};
+
+use itertools::Itertools;
 
 advent_of_code::solution!(8);
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Node<'a> {
     name: &'a str,
     left_path: &'a str,
@@ -23,17 +25,32 @@ impl<'a> Node<'a> {
     }
 }
 
-fn traverse_land(patern: &str, map: HashMap<&str, Node>) -> u32 {
-    let mut steps: usize = 0;
-    let mut next_node = "AAA";
+fn all_node_finish(nodes: &[&str]) -> bool {
+    nodes.iter().all(|&s| s.ends_with('Z'))
+}
 
-    while next_node != "ZZZ" {
-        match patern.chars().nth(steps % patern.len()).unwrap() {
-            'L' => next_node = map.get(next_node).unwrap().left_path,
-            'R' => next_node = map.get(next_node).unwrap().right_path,
-            _ => todo!(),
-        }
+fn traverse_land(patern: &str, start_node: Vec<&str>, map: HashMap<&str, Node>) -> u32 {
+    let mut steps: usize = 0;
+    let mut next_node: Vec<_> = start_node;
+
+    while !all_node_finish(&next_node) {
+        // println!("{:?}", next_node);
+
+        next_node = next_node
+            .iter()
+            .map(
+                |&s| match patern.chars().nth(steps % patern.len()).unwrap() {
+                    'L' => map.get(s).unwrap().left_path,
+                    'R' => map.get(s).unwrap().right_path,
+                    _ => todo!(),
+                },
+            )
+            .collect();
+
         steps += 1;
+        if steps % 1000000 == 0 {
+            println!("{:?}:  {:?}", steps, next_node);
+        }
     }
     steps as u32
 }
@@ -50,13 +67,37 @@ pub fn part_one(input: &str) -> Option<u32> {
         hash.insert(node.name, node);
     });
 
-    let res = traverse_land(patern, hash);
+    let node: Vec<&str> = vec!["AAA"];
+    // println!("{:?}", node);
+
+    let res = traverse_land(patern, node, hash);
 
     Some(res)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut lines_iter = input.lines();
+    let patern = &lines_iter.next().unwrap();
+    lines_iter.next();
+
+    let mut hash: HashMap<&str, Node> = HashMap::new();
+
+    lines_iter.for_each(|line| {
+        let node = Node::new(line);
+        hash.insert(node.name, node);
+    });
+
+    let node: Vec<_> = hash
+        .clone()
+        .into_keys()
+        .filter(|&k| k.ends_with('A'))
+        .collect();
+
+    println!("{:?}", node);
+
+    let res = traverse_land(patern, node, hash);
+
+    Some(res)
 }
 
 #[cfg(test)]
@@ -72,6 +113,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
