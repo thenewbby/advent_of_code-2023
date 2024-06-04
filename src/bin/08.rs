c7be7230eb182a5d3c92fbf0ca91887611f720c8
TheNewby <alx.care@gmail.com>
2024-06-05 00:35:27 +0200

    solve 8

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -24,38 +24,82 @@ impl<'a> Node<'a> {
         }
     }
 }
-
-fn all_node_finish(nodes: &[&str]) -> bool {
-    nodes.iter().all(|&s| s.ends_with('Z'))
+#[derive(Debug)]
+struct Ghost<'a> {
+    pos: &'a str,
+    cycle: Option<usize>,
 }
 
-fn traverse_land(patern: &str, start_node: Vec<&str>, map: HashMap<&str, Node>) -> u32 {
-    let mut steps: usize = 0;
-    let mut next_node: Vec<_> = start_node;
+fn all_node_finish(gh: &[Ghost]) -> bool {
+    gh.iter().all(|g| g.pos.ends_with('Z'))
+}
 
-    while !all_node_finish(&next_node) {
-        // println!("{:?}", next_node);
+fn all_ghosts_cycled(gh: &[Ghost]) -> bool {
+    gh.iter().all(|g| g.cycle.is_some())
+}
 
-        next_node = next_node
-            .iter()
-            .map(
-                |&s| match patern.chars().nth(steps % patern.len()).unwrap() {
-                    'L' => map.get(s).unwrap().left_path,
-                    'R' => map.get(s).unwrap().right_path,
-                    _ => todo!(),
-                },
-            )
-            .collect();
+fn traverse_land<'a>(patern: &str, ghosts: &mut Vec<Ghost<'a>>, map: HashMap<&str, Node<'a>>) {
+    let mut cycle: usize = 1;
 
-        steps += 1;
-        if steps % 1000000 == 0 {
-            println!("{:?}:  {:?}", steps, next_node);
-        }
+    // println!("{:?}", ghosts);
+    while !all_ghosts_cycled(&ghosts) {
+        ghosts
+            .iter_mut()
+            .filter(|g| g.cycle.is_none())
+            .for_each(|g| {
+                // match patern.chars().nth(steps % patern.len()).unwrap() {
+                patern.chars().for_each(|c| {
+                    match c {
+                        'L' => g.pos = map.get(g.pos).unwrap().left_path,
+                        'R' => g.pos = map.get(g.pos).unwrap().right_path,
+                        _ => todo!(),
+                    };
+                });
+                if g.pos.ends_with('Z') {
+                    g.cycle = Some(cycle);
+                    // println!("{:?}", g);
+                }
+            });
+        // println!("{:?}", ghosts);
+
+        cycle += 1;
     }
-    steps as u32
+
+    // while !all_node_finish(&ghosts) {
+    //     // println!("{:?}", next_node);
+
+    //     ghosts
+    //         .iter()
+    //         .map(
+    //             |&s| match patern.chars().nth(steps % patern.len()).unwrap() {
+    //                 'L' => map.get(s.pos).unwrap().left_path,
+    //                 'R' => map.get(s.pos).unwrap().right_path,
+    //                 _ => todo!(),
+    //             },
+    //         )
+    //         .collect();
+
+    //     steps += 1;
+    //     if steps % 1000000 == 0 {
+    //         println!("{:?}:  {:?}", steps, next_node);
+    //     }
+    // }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    while b != 0 {
+        let tmp = a;
+        a = b;
+        b = tmp % b;
+    }
+    a
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
     let mut lines_iter = input.lines();
     let patern = &lines_iter.next().unwrap();
     lines_iter.next();
@@ -67,15 +111,20 @@ pub fn part_one(input: &str) -> Option<u32> {
         hash.insert(node.name, node);
     });
 
-    let node: Vec<&str> = vec!["AAA"];
-    // println!("{:?}", node);
+    let mut ghosts: Vec<Ghost> = vec![Ghost {
+        pos: "AAA",
+        cycle: None,
+    }];
+    traverse_land(patern, &mut ghosts, hash);
 
-    let res = traverse_land(patern, node, hash);
-
-    Some(res)
+    let min_shared_cycles = ghosts
+        .into_iter()
+        .filter_map(|ghost| ghost.cycle)
+        .fold(1, lcm);
+    Some(min_shared_cycles * patern.len())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<usize> {
     let mut lines_iter = input.lines();
     let patern = &lines_iter.next().unwrap();
     lines_iter.next();
@@ -87,17 +136,27 @@ pub fn part_two(input: &str) -> Option<u32> {
         hash.insert(node.name, node);
     });
 
-    let node: Vec<_> = hash
+    let mut ghosts: Vec<_> = hash
         .clone()
         .into_keys()
         .filter(|&k| k.ends_with('A'))
+        .map(|k| Ghost {
+            pos: k,
+            cycle: None,
+        })
         .collect();
 
-    println!("{:?}", node);
+    // println!("{:?}", ghosts);
 
-    let res = traverse_land(patern, node, hash);
+    traverse_land(patern, &mut ghosts, hash);
+    // println!("{:?}", ghosts);
+    let min_shared_cycles = ghosts
+        .into_iter()
+        .filter_map(|ghost| ghost.cycle)
+        .fold(1, lcm);
+    println!("{:?}", min_shared_cycles);
 
-    Some(res)
+    Some(min_shared_cycles * patern.len())
 }
 
 #[cfg(test)]
