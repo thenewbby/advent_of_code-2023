0f25fbf57b9210f065b2a869b38fa51c7b709056
TheNewby <alx.care@gmail.com>
2024-06-05 22:26:12 +0200

    solve 9 part two

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -8,21 +8,41 @@ fn generate_next_line(line: &[i64]) -> Vec<i64> {
     line.windows(2).map(|slice| slice[1] - slice[0]).collect()
 }
 
-fn back_propagate(workspace: &mut [Vec<i64>]) {
+fn front_propagate(workspace: &mut [Vec<i64>]) {
     for i in (0..workspace.len() - 1).rev() {
         workspace[i].push(workspace[i].last().unwrap() + workspace[i + 1].last().unwrap())
     }
 }
 
-fn extrapolate(history: Vec<i64>) -> i64 {
+// fn back_propagate(workspace: &mut [Vec<i64>]) {
+//     for i in (0..workspace.len() - 1).rev() {
+//         workspace[i].push(workspace[i].last().unwrap() + workspace[i + 1].last().unwrap())
+//     }
+// }
+
+fn frontrapolate(history: Vec<i64>) -> i64 {
     let mut workspace: Vec<Vec<_>> = vec![history];
+    // println!("{:?}", workspace);
+
+    while workspace.last().unwrap().iter().any(|v| !v.is_zero()) {
+        workspace.push(generate_next_line(workspace.last().unwrap()));
+    }
+    front_propagate(workspace.as_mut_slice());
+    // println!("{:?}", workspace);
+
+    workspace[0].last().unwrap().to_owned()
+}
+
+fn backapolate(history: Vec<i64>) -> i64 {
+    let mut workspace: Vec<Vec<_>> = vec![history];
+    workspace[0].reverse();
 
     // println!("{:?}", workspace);
 
     while workspace.last().unwrap().iter().any(|v| !v.is_zero()) {
         workspace.push(generate_next_line(workspace.last().unwrap()));
     }
-    back_propagate(workspace.as_mut_slice());
+    front_propagate(workspace.as_mut_slice());
     // println!("{:?}", workspace);
 
     workspace[0].last().unwrap().to_owned()
@@ -40,11 +60,22 @@ pub fn part_one(input: &str) -> Option<i64> {
 
     // println!("{:?}", histories);
 
-    Some(histories.iter().map(|v| extrapolate(v.to_owned())).sum())
+    Some(histories.iter().map(|v| frontrapolate(v.to_owned())).sum())
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let histories: Vec<Vec<_>> = input
+        .lines()
+        .map(|l| {
+            l.split_ascii_whitespace()
+                .map(|v| v.parse::<i64>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    // println!("{:?}", histories);
+
+    Some(histories.iter().map(|v| backapolate(v.to_owned())).sum())
 }
 
 #[cfg(test)]
@@ -54,12 +85,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(18));
+        assert_eq!(result, Some(114));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 }
