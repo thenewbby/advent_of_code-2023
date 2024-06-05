9ec922c7b9156f0e724f44832925fa504e489e15
TheNewby <alx.care@gmail.com>
2024-06-05 22:03:10 +0200

    solve 9 part one

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,10 +1,49 @@
+use std::vec;
+
+use pathfinding::num_traits::Zero;
+
 advent_of_code::solution!(9);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+fn generate_next_line(line: &[i64]) -> Vec<i64> {
+    line.windows(2).map(|slice| slice[1] - slice[0]).collect()
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+fn back_propagate(workspace: &mut [Vec<i64>]) {
+    for i in (0..workspace.len() - 1).rev() {
+        workspace[i].push(workspace[i].last().unwrap() + workspace[i + 1].last().unwrap())
+    }
+}
+
+fn extrapolate(history: Vec<i64>) -> i64 {
+    let mut workspace: Vec<Vec<_>> = vec![history];
+
+    // println!("{:?}", workspace);
+
+    while workspace.last().unwrap().iter().any(|v| !v.is_zero()) {
+        workspace.push(generate_next_line(workspace.last().unwrap()));
+    }
+    back_propagate(workspace.as_mut_slice());
+    // println!("{:?}", workspace);
+
+    workspace[0].last().unwrap().to_owned()
+}
+
+pub fn part_one(input: &str) -> Option<i64> {
+    let histories: Vec<Vec<_>> = input
+        .lines()
+        .map(|l| {
+            l.split_ascii_whitespace()
+                .map(|v| v.parse::<i64>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    // println!("{:?}", histories);
+
+    Some(histories.iter().map(|v| extrapolate(v.to_owned())).sum())
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
     None
 }
 
@@ -15,7 +54,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(18));
     }
 
     #[test]
